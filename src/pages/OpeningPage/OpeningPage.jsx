9092f9a4aa358163351602390c3c3cf436d58fbe
Keshet302 <mador.till@gmail.com>
2026-03-09 22:41:21 +0200

    09/03/26 evening 2

--- src/pages/OpeningPage/OpeningPage.jsx
+++ src/pages/OpeningPage/OpeningPage.jsx
@@ -10,51 +10,120 @@ import logo from "../../assets/logo.png";
 import tillBlackLogo from "../../assets/till_blacklogo.svg";
 import bushLeft from "../../assets/bush-left.svg";
 import bushRight from "../../assets/bush-right.svg";
+import topic1 from "../../assets/car-plate-topic-1.svg";
+import topic2 from "../../assets/car-plate-topic-2.svg";
 
 function OpeningPage() {
     const navigate = useNavigate();
     const [inTopics, setInTopics] = useState(false);
+    const [driveMode, setDriveMode] = useState(false);
 
     const handleToTopics = () => {
-        setInTopics(true);
+        setDriveMode(true);
+
+        setTimeout(() => {
+            setInTopics(true);
+        }, 2000);
     };
 
     return (
         <div className="opening-page">
 
-            <img src={road} alt="road" className="road-opening-page" />
-            <img src={lightingPole} alt="lighting pole" className="lighting-pole-opening-page-left" />
-            <img src={lightingPole} alt="lighting pole" className="lighting-pole-opening-page-right" />
+            <div className="ground-area">
+                <img src={road} className="road-opening-page" />
+
+                <img
+                    src={lightingPole}
+                    className={`lighting-pole-left ${driveMode ? "pole-move-left" : ""}`}
+                />
+
+                <img
+                    src={lightingPole}
+                    className={`lighting-pole-right ${driveMode ? "pole-move-right" : ""}`}
+                />
+
+                <img src={bushLeft} className="bush-left" />
+                <img src={bushRight} className="bush-right" />
+
+                <img
+                    src={car}
+                    className={`car-opening-page ${driveMode ? "car-drive" : ""}`}
+                />
+            </div>
+
             <img src={BigCloud} alt="big cloud" className="big-cloud-opening-page-left" />
             <img src={BigCloud} alt="big cloud" className="big-cloud-opening-page-right" />
             <img src={SmallCloud} alt="small cloud" className="small-cloud-opening-page-left" />
             <img src={SmallCloud} alt="small cloud" className="small-cloud-opening-page-right" />
-            <img src={car} alt="car" className="car-opening-page" />
-            <img src={logo} alt="logo bahad 13" className="logo-bahad13-opening-page" />
             <img src={tillBlackLogo} alt="till logo" className="till-logo-black-opening-page" />
-            <img src={bushLeft} alt="bush left" className="bush-left-opening-page" />
-            <img src={bushRight} alt="bush right" className="bush-right-opening-page" />
 
-            {!inTopics && <>
-                <h1 className="opening-page-header">ברוכים הבאים ללומדת תעבורה</h1>
+            {/* {!inTopics &&
+                <div className="opening-content">
 
-                <button onClick={handleToTopics} className="start-btn-to-topics">להתחלת הלומדה</button>
-            </>}
+                    <img src={logo} alt="logo bahad 13" className="logo-bahad13-opening-page" />
+
+                    <h1 className="opening-page-header">
+                        ברוכים הבאים ללומדת תעבורה
+                    </h1>
+
+                    <button onClick={handleToTopics} className="start-btn-to-topics">
+                        להתחלת הלומדה
+                    </button>
+
+                </div>}
 
             {inTopics && <>
-                <div className="topics">
-                    <p>נלמד היום:</p>
-                    <ul>
-                        <li>זיהוי כלי רכב</li>
-                        <li>לוחיות רישוי</li>
-                        <li>התנהגות בכביש</li>
-                    </ul>
-                </div>
+                <img src={logo} alt="logo bahad 13" className="logo-bahad13-opening-page-topics" />
 
-                <button onClick={() => navigate("/learning")}>
-                    התחל
+                <div className="opening-content">
+                    <div className="topics">
+                        <p className="topics-opening-page-header">מה נלמד היום?</p>
+                        <div className="topics-opening-page-container">
+                            <img src={topic1} className="topic" alt="topic 1" />
+                            <img src={topic2} className="topic" alt="topic 2" />
+                        </div>
+                    </div>
+
+                    <button onClick={() => navigate("/learning")} className="start-btn-to-topics" >
+                        בואו נתחיל!
+                    </button>
+                </div>
+            </>} */}
+
+            <div className={`opening-content first ${inTopics ? "fade-out" : "fade-in"}`}>
+                <img src={logo} className="logo-bahad13-opening-page" />
+
+                <h1 className="opening-page-header">
+                    ברוכים הבאים ללומדת תעבורה
+                </h1>
+
+                <button onClick={handleToTopics} className="start-btn-to-topics">
+                    להתחלת הלומדה
                 </button>
-            </>}
+            </div>
+
+
+            <div className={`topics-screen ${inTopics ? "fade-in" : "hidden"}`}>
+                <img src={logo} className="logo-bahad13-opening-page-topics" />
+
+                <div className="opening-content-topics">
+                    <div className="topics">
+                        <p className="topics-opening-page-header">מה נלמד היום?</p>
+
+                        <div className="topics-opening-page-container">
+                            <img src={topic1} className="topic" />
+                            <img src={topic2} className="topic" />
+                        </div>
+                    </div>
+
+                    <button
+                        onClick={() => navigate("/learning")}
+                        className="start-btn-to-topics"
+                    >
+                        בואו נתחיל!
+                    </button>
+                </div>
+            </div>
 
         </div>
     );
