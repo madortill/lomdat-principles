d1b4d4ce74b2376dbcdbe4da0ba7a3d2242f9863
madortill <mador.till@gmail.com>
2026-03-23 17:01:54 +0200

    23/03/26 #2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,3 @@
-// import React from "react";
-// import { useState } from "react";
-// import "./css/App.css";
-
-// function App() {
-
-//   return (
-//     <>
-//       <div className="app">
-//       hello!
-//       <img src="./vite.svg" alt="vite" />
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import OpeningPage from "./pages/OpeningPage/OpeningPage";
 import LearningPage from "./pages/LearningPage/LearningPage";
