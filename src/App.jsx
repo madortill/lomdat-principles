aaa42f3e341bd416411e6844e959df9fc9e3e83e
madortill <mador.till@gmail.com>
2026-05-05 10:53:01 +0300

    05/05/26

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,72 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { useState, useEffect } from "react";
+// import MobileBlocker from "./components/MobileBlocker/MobileBlocker";
+// import OpeningPage from "./pages/OpeningPage/OpeningPage";
+// import LearningPage from "./pages/LearningPage/LearningPage";
+// import LearningPage2 from "./pages/LearningPage2/LearningPage2";
+// import EndPage from "./pages/EndPage/EndPage";
+// import "./App.css";
+
+// function App() {
+//   const [isMobile, setIsMobile] = useState(false);
+
+//   useEffect(() => {
+//     function checkScreen() {
+//       setIsMobile(window.innerWidth < 900);
+//     }
+//     checkScreen();
+
+//     window.addEventListener("resize", checkScreen);
+//     return () => window.removeEventListener("resize", checkScreen);
+//   }, []);
+
+//   // נתונים של פרק 1
+//   const [ch1Progress, setCh1Progress] = useState({
+//     currentSlide: 0,
+//     maxVisited: 0,
+//     completed: {},
+//   });
+
+//   // נתונים של פרק 2
+//   const [ch2Progress, setCh2Progress] = useState({
+//     currentSlide: 0,
+//     maxVisited: 0,
+//     completed: {},
+//   });
+
+//   if (isMobile) {
+//     return <MobileBlocker />;
+//   }
+
+//   return (
+//     <BrowserRouter basename="/lomdat-principles">
+//       <Routes>
+//         <Route path="/" element={<OpeningPage />} />
+//         <Route
+//           path="/learning"
+//           element={
+//             <LearningPage progress={ch1Progress} setProgress={setCh1Progress} />
+//           }
+//         />
+//         <Route
+//           path="/learning2"
+//           element={
+//             <LearningPage2
+//               progress={ch2Progress}
+//               setProgress={setCh2Progress}
+//               ch1Max={ch1Progress.maxVisited}
+//             />
+//           }
+//         />
+//         <Route path="/end" element={<EndPage />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 import MobileBlocker from "./components/MobileBlocker/MobileBlocker";
 import OpeningPage from "./pages/OpeningPage/OpeningPage";
@@ -17,13 +85,8 @@ function App() {
     checkScreen();
 
     window.addEventListener("resize", checkScreen);
-
     return () => window.removeEventListener("resize", checkScreen);
   }, []);
-
-  if (isMobile) {
-    return <MobileBlocker />;
-  }
 
   // נתונים של פרק 1
   const [ch1Progress, setCh1Progress] = useState({
@@ -39,8 +102,12 @@ function App() {
     completed: {},
   });
 
+  if (isMobile) {
+    return <MobileBlocker />;
+  }
+
   return (
-    <BrowserRouter basename="/lomdat-principles">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<OpeningPage />} />
         <Route
@@ -61,7 +128,7 @@ function App() {
         />
         <Route path="/end" element={<EndPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
